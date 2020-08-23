9b55f5236ac6f48dc73d25be895e6dd4a423b793
ikanago <28985004+ikanago@users.noreply.github.com>
2020-08-23 16:26:31 +0900

    fix: To run on Cloud Run properly, listen on PORT

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -62,6 +63,9 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+
+	port := os.Getenv("PORT")
+	http.ListenAndServe(port, nil)
 
 	log.Println("Bot is running")
 	sc := make(chan os.Signal, 1)
