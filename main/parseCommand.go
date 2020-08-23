e89dd5b4a753602a76163bf93745b573fa4806aa
ikanago <28985004+ikanago@users.noreply.github.com>
2020-08-23 21:36:49 +0900

    feat: Add help message

--- main/parseCommand.go
+++ main/parseCommand.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"log"
 	"strings"
 )
 
 func ParseCommand(message string) []string {
 	arguments := strings.Fields(message)
-	log.Println(len(arguments))
 	return arguments
 }
